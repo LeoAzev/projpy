b75e3757fd6621b872827b2afbfd974731e5672f
Leonardo de Azevedo <leomsaz@hotmail.com>
2024-10-21 16:23:14 -0300

    add new teste

---
+++ new_app.py
@@ -0,0 +1,14 @@
+#​Código teste para debug 
+#breakpoint "F9" marca a linha para verificar o erro, "debug"
+
+print('Olá')
+
+def calcular_preco_combo(pizza, refrigerante):
+
+    total = pizza + refrigerante
+
+    print(total)
+
+calcular_preco_combo(30, 20)
+
+print('Programa finalizado')
